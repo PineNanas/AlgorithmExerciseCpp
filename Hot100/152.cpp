ba5d4f29bb674c3424482484173af1abf83ce6b6
PineNanas <pinenanas@outlook.com>
2026-03-18 15:38:17 +0800

    152

--- Hot100/152.cpp
+++ Hot100/152.cpp
@@ -38,3 +38,57 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> dp(n + 1, {INT_MAX, INT_MIN});
+        dp[0].first = 1;
+        dp[0].second = 1;
+        int ans = INT_MIN;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                dp[i + 1].first = max(nums[i], nums[i] * dp[i].first);
+                dp[i + 1].second = min(nums[i], nums[i] * dp[i].second);
+            }
+            else
+            {
+                dp[i + 1].first = max(nums[i], nums[i] * dp[i].second);
+                dp[i + 1].second = min(nums[i], nums[i] * dp[i].first);
+            }
+            ans = max(ans, dp[i + 1].first);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxCur = 1;
+        int minCur = 1;
+        int ans = INT_MIN;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                maxCur = max(nums[i], nums[i] * maxCur);
+                minCur = min(nums[i], nums[i] * minCur);
+            }
+            else
+            {
+                int tmp = maxCur;
+                maxCur = max(nums[i], nums[i] * minCur);
+                minCur = min(nums[i], nums[i] * tmp);
+            }
+            ans = max(ans, maxCur);
+        }
+        return ans;
+    }
+};
